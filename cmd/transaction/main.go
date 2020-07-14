a208cff3da2e0a002879e884a7fedda9e5158c2e
britishpop <drmakc@gmail.com>
2020-07-14 10:41:44 +0300

    Раскомментированы вызовы

--- cmd/transaction/main.go
+++ cmd/transaction/main.go
@@ -27,28 +27,28 @@ func main() {
 
 	transactions := transaction.MakeTransactions(1)
 
-	// // первая функция
-	// fmt.Println("Результаты первой функции")
-	// sum := transaction.SumByMCC(transactions, 1)
-	// for k, v := range sum {
-	// 	fmt.Printf("Sum transaction in %s: %d \r\n", k, v)
-	// }
+	// первая функция
+	fmt.Println("Результаты первой функции")
+	sum := transaction.SumByMCC(transactions, 1)
+	for k, v := range sum {
+		fmt.Printf("Sum transaction in %s: %d \r\n", k, v)
+	}
 	// вторая функция
 	fmt.Println("Результаты второй функции")
 	sum2 := transaction.MutexSumByMCC(transactions, 1)
 	for k, v := range sum2 {
 		fmt.Printf("Sum transaction in %s: %d \r\n", k, v)
 	}
-	// // третья функция
-	// fmt.Println("Результаты третьей функции")
-	// sum3 := transaction.ChanSumByMCC(transactions, 1)
-	// for k, v := range sum3 {
-	// 	fmt.Printf("Sum transaction in %s: %d \r\n", k, v)
-	// }
-	// // четвертая функция
-	// fmt.Println("Результаты четвертой функции")
-	// sum4 := transaction.MutexSumByMCC2(transactions, 1)
-	// for k, v := range sum4 {
-	// 	fmt.Printf("Sum transaction in %s: %d \r\n", k, v)
-	// }
+	// третья функция
+	fmt.Println("Результаты третьей функции")
+	sum3 := transaction.ChanSumByMCC(transactions, 1)
+	for k, v := range sum3 {
+		fmt.Printf("Sum transaction in %s: %d \r\n", k, v)
+	}
+	// четвертая функция
+	fmt.Println("Результаты четвертой функции")
+	sum4 := transaction.MutexSumByMCC2(transactions, 1)
+	for k, v := range sum4 {
+		fmt.Printf("Sum transaction in %s: %d \r\n", k, v)
+	}
 }
